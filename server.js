e4bffdabfe77ad1fcb1119f6721d97df61ac5d59
Ciaran Hayes <hayes.c.hayes@gmail.com>
2025-04-10 16:39:09 +0200

    Fixed placement

--- server.js
+++ server.js
@@ -7,11 +7,11 @@ import cors from "cors";
 dotenv.config();
 connectDB();
 const port = process.env.PORT || 3000;
-app.use(cors());
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+app.use(cors());
 
 app.get('/', (req, res) => {
     res.send("Welcome to the Queer Book Repository");
