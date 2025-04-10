0b131ec2685fb12e13ef94971a4e0c5c6a4db1cc
Ciaran Hayes <hayes.c.hayes@gmail.com>
2025-04-10 16:31:35 +0200

    cors added

--- server.js
+++ server.js
@@ -2,10 +2,12 @@ import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./database/db.js";
 import bookRouter from "./routes/booksRoutes.js"
+import cors from cors;
 
 dotenv.config();
 connectDB();
 const port = process.env.PORT || 3000;
+app.use(cors());
 
 const app = express();
 app.use(express.json());
