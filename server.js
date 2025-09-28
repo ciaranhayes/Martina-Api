e01fd7dc807ea7f6428322fce37336f9ffc6feb9
Ciaran Hayes <hayes.c.hayes@gmail.com>
2025-09-28 17:42:32 +0200

    Log in

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./database/db.js";
-import blogRouter from "./routes/blogRouter.js"
+import blogRouter from "./routes/blogRouter.js";
 import cors from "cors";
 
 dotenv.config();
@@ -10,15 +10,28 @@ const port = process.env.PORT || 3000;
 
 const app = express();
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
-app.get('/', (req, res) => {
-    res.send("Welcome to the blog");
+app.get("/", (req, res) => {
+  res.send("Welcome to the blog");
+});
+
+const USER_EMAIL = process.env.USER_EMAIL;
+const USER_PASSWORD = process.env.USER_PASSWORD;
+
+app.post("/login", (req, res) => {
+  const { email, password } = req.body;
+
+  if (email === USER_EMAIL && password === USER_PASSWORD) {
+    return res.json({ success: true, message: "Login successful!" });
+  }
+
+  res.status(401).json({ success: false, message: "Invalid credentials" });
 });
 
 app.use("/", blogRouter);
 
 app.listen(port, () => {
-    console.log(`Running on port: ${port}`);
+  console.log(`Running on port: ${port}`);
 });
